7686b7278504d5dba0e9726c48d2a6c5aa75640e
vipmengfeng <vipmengfeng@163.com>
2013-12-03 22:42:49 +0800

    密码记事本

---
+++ FeiFeikeyTests/FeiFeikeyTests.m
@@ -0,0 +1,34 @@
+//
+//  FeiFeikeyTests.m
+//  FeiFeikeyTests
+//
+//  Created by meng on 13-12-2.
+//  Copyright (c) 2013年 meng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FeiFeikeyTests : XCTestCase
+
+@end
+
+@implementation FeiFeikeyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
